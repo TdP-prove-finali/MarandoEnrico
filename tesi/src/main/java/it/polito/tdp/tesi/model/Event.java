c959d7c7e556d380c8400145cc16f47c6621be50
enricomarando <enricomarando@localhost>
2020-12-29 09:47:42 +0100

    bozza finale

--- tesi/src/main/java/it/polito/tdp/tesi/model/Event.java
+++ tesi/src/main/java/it/polito/tdp/tesi/model/Event.java
@@ -6,18 +6,15 @@ public class Event implements Comparable <Event> {
 	
 	
 	public enum EventType {
-		ACQUISTO, ORDINE; //semplice acquisto oppure 'ordine' nel caso si fosse già tentato un acquisto la settimana precedente e si decide di tornare la settimana eseguente essendo il prodotto momentaneamente terminato
-	}
+		ACQUISTO; }
 	
 	private int week;
-	private EventType type;
 	private String tipologia; //tipologia di prodotto da acquistare
 	
 
 	public Event(int week, EventType type, String tipologia) {
 		super();
 		this.week = week;
-		this.type = type;
 		this.tipologia = tipologia;
 	}
 
@@ -26,13 +23,6 @@ public class Event implements Comparable <Event> {
 
 	public int getWeek() {
 		return week;
-	}
-
-
-
-
-	public EventType getType() {
-		return type;
 	}
 
 
@@ -56,7 +46,7 @@ public class Event implements Comparable <Event> {
 
 	@Override
 	public String toString() {
-		return week + " " + type + " " + tipologia;
+		return week + " " + tipologia;
 	}
 	
 	
--- tesi/src/main/java/it/polito/tdp/tesi/model/Simulator.java
+++ tesi/src/main/java/it/polito/tdp/tesi/model/Simulator.java
@@ -6,20 +6,34 @@ import it.polito.tdp.tesi.model.Event.EventType;
 
 public class Simulator {
 	
-	int libririm; //libri rimasti in magazzino
-	int closerim; //close-up rimasti in magazzino
-	int collezionabilirim; //collezionabili rimasti in magazzino
-	int scenarim; //scena rimasti in magazzino
+	int libriRim; //libri rimasti in magazzino
+	int closeRim; //close-up rimasti in magazzino
+	int collezionabiliRim; //collezionabili rimasti in magazzino
+	int scenaRim; //scena rimasti in magazzino
+	
+	//quantità massime in magazzino
+		int libriMax;
+		int closeMax;
+		int collezionabiliMax;
+		int scenaMax;
+
 	
 	private PriorityQueue<Event> queue = new PriorityQueue<>();
-	private int soddisfatti;
+	private int soddisfatti; //contatore per i clienti che riescono ad acquistare
+	
+	private int prenotatiL=0; //libri prenotati per la settimana successiva
+	private int prenotatiC=0; //close up prenotati per la settimana successiva
+	private int prenotatiCl=0; //collezionabili prenotati per la settimana successiva
+	private int prenotatiS=0; //scena prenotati per la settimana successiva
 	
 	public void run(int libri, int close, int collezionabili, int scena) {
+		
 		//all'inizio le rimanenze per ogni reparto sono uguali alla loro capienza massima (scelta dall'utente) 
-		libririm = libri; 
-		closerim = close;
-		collezionabilirim = collezionabili;
-		scenarim = scena;
+			libriRim = libriMax = libri; 
+			closeRim = closeMax = close;
+			collezionabiliRim = collezionabiliMax = collezionabili;
+			scenaRim = scenaMax = scena;
+			
 		int i=0;
 	
 		for (int j=0; j<4; j++) {  //ipotesi di 4 settimane
@@ -68,47 +82,92 @@ public class Simulator {
 	
 	private void processEvent(Event e) {
 		
-		switch(e.getType()) {
+		boolean nuovaSettimana = false;
 		
-		case ACQUISTO:
-			
+		if (this.queue.iterator().hasNext()) {
+			if (this.queue.iterator().next().getWeek() != e.getWeek()) {
+				nuovaSettimana = true;
+			}
+		}
+		
 			switch (e.getTipologia()) {
 			
 			case "libri":
+				if (libriRim>0) {
+					libriRim--;
+					soddisfatti++;
+					
+				}
+				else {
+					if (Math.random()>0.4) //probabilità che il clienti ordini il prodotto per la settimana prossima
+						prenotatiL++;
+					if (prenotatiL<=libriMax) //in caso contrario le prenotazioni per la settimana seguente hanno già raggiunto il numero massimo che avrò in magazzino ed ho quindi esaurito i prodotti anche per la settimana prossima
+						soddisfatti++;
+				}
 				break;
 				
 			case "collezionabili":
+				if (collezionabiliRim>0) {
+					collezionabiliRim--;
+					soddisfatti++;
+					
+				}
+				else {
+					if (Math.random()>0.4) //probabilità che il clienti ordini il prodotto per la settimana prossima
+						prenotatiCl++;
+					if (prenotatiCl<=collezionabiliMax) //in caso contrario le prenotazioni per la settimana seguente hanno già raggiunto il numero massimo che avrò in magazzino ed ho quindi esaurito i prodotti anche per la settimana prossima
+						soddisfatti++;
+				}
 				break;
 				
 			case "close":
+				if (closeRim>0) {
+					closeRim--;
+					soddisfatti++;
+					
+				}
+				else {
+					if (Math.random()>0.4) //probabilità che il clienti ordini il prodotto per la settimana prossima
+						prenotatiC++;
+					if (prenotatiC<=closeMax) //in caso contrario le prenotazioni per la settimana seguente hanno già raggiunto il numero massimo che avrò in magazzino ed ho quindi esaurito i prodotti anche per la settimana prossima
+						soddisfatti++;
+				}
 				break;
 				
 			case "scena":
+				if (scenaRim>0) {
+					scenaRim--;
+					soddisfatti++;
+					
+				}
+				else {
+					if (Math.random()>0.4) //probabilità che il clienti ordini il prodotto per la settimana prossima
+						prenotatiS++;
+					if (prenotatiS<=scenaMax) //in caso contrario le prenotazioni per la settimana seguente hanno già raggiunto il numero massimo che avrò in magazzino ed ho quindi esaurito i prodotti anche per la settimana prossima
+						soddisfatti++;
+				}
 				break;	
 				
 			}
-	
-			break;
 			
-		case ORDINE:
-			
-			switch (e.getTipologia()) {
-			
-				case "libri":
-					break;
-				
-				case "collezionabili":
-					break;
-				
-				case "close":
-					break;
-				
-				case "scena":
-					break;	
-				
-			}
-
-			break;
-}
+	//il prossimo evento sarà il primo della prossima settimana perciò il magazzino torna a capienza massima meno i prodotti già prenotati che vengono acquistati ma tenuti da parte per evitare di essere venduti ad altre persone
+	if (nuovaSettimana==true) { 
+		libriRim=libriMax-prenotatiL;
+		prenotatiL=0;
+		
+		collezionabiliRim=collezionabiliMax-prenotatiCl;
+		prenotatiCl=0;
+		
+		closeRim=closeMax-prenotatiC;
+		prenotatiC=0;
+		
+		scenaRim=scenaMax-prenotatiS;
+		prenotatiS=0;
 	}
 }
+	public int getSoddisfatti() {
+		return this.soddisfatti/120*100;
+	}
+	
+	}
+
